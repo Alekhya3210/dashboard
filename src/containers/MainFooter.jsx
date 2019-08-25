736e6aaa460b4a1630a72c864f6153e5b966b5ef
Alekhya3210 <31490336+Alekhya3210@users.noreply.github.com>
2019-08-25 00:05:02 -0400

    Create MainFooter.jsx

---
+++ src/containers/MainFooter.jsx
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const MainFooter = () => (
+  <Fragment>
+    <span><a href="https://github.com/JamesSingleton" target="_blank" rel="noopener noreferrer">James Singleton</a> &copy; 2019</span>
+    <span className="ml-auto">Powered by <a href="https://github.com/JamesSingleton/react-building-blocks" rel="noopener noreferrer" target="_blank">React Building Blocks</a></span>
+  </Fragment>
+);
+
+export default MainFooter;
